9242779937f3e3e563dbee1b144028ae21a42d79
Gaurav23K <42205976+Gaurav23K@users.noreply.github.com>
2018-08-17 02:06:51 -0400

    Add files via upload

---
+++ thirdweek2nd.cpp
@@ -0,0 +1,47 @@
+// included library
+# include <iostream>
+using namespace std;
+
+//DIVISION
+
+int main() {
+float a,b, result;
+a=48;
+b=5;
+result =a/b;
+cout<< result << endl;
+
+//ADDITION
+
+int a1,b1,radd;
+a1=23;
+b1=5;
+radd = a1+b1;
+cout<<radd<<endl;
+
+//SUBSTRACTION
+
+int a2,b2,rsub;
+a2=22;
+b2=11;
+rsub = a2-b2;
+cout<<rsub<<endl;
+
+// MULTIPLICATION
+
+float a3,b3,rmult;
+a3=25.5;
+b3=10;
+rmult=a3*b3;
+cout<<rmult<<endl;
+
+//MODULUS
+
+int a4,b4,rmodu;
+a4=56;
+b4=23;
+rmodu=a4%b4;
+cout<<rmodu<<endl;
+return 11;
+
+}
