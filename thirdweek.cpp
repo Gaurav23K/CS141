ab0abba8f38e032997d47b277dd5924cfc6b4fa1
Gaurav23K <42205976+Gaurav23K@users.noreply.github.com>
2018-08-17 01:30:37 -0400

    Add files via upload

---
+++ thirdweek.cpp
@@ -0,0 +1,20 @@
+// included library
+# include <iostream>
+using namespace std;
+// main function
+int main()
+{
+//declaring values
+int a=55500;
+char b=55;
+float c=1.02;
+double d=2.015;
+bool e=4;
+//size
+cout<<sizeof(a)<<endl;
+cout<<sizeof(b)<<endl;
+cout<<sizeof(c)<<endl;
+cout<<sizeof(d)<<endl;
+cout<<sizeof(e)<<endl;
+
+}
